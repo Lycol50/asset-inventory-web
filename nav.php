34920a27867d0f6a6ab59ccc4d1c4cbd87b696e0
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-30 05:48:41 +0000

    nav

--- nav.php
+++ nav.php
@@ -3,7 +3,7 @@ $register = '<a class="nav-item nav-link" href="register_user.php">Register User
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark noprint">
   <a class="navbar-brand" href="#" style="font-family: Kanit, sans-serif;">Asset Management System</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
