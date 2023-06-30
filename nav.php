9d6591b1b81be439aaea5c591b0ef714e1d49f8c
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-30 05:28:18 +0000

    nav

--- nav.php
+++ nav.php
@@ -1,3 +1,6 @@
+<?php
+$register = '<a class="nav-item nav-link" href="register_user.php">Register User</a>';
+?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark noprint">
   <a class="navbar-brand" href="#" style="font-family: Kanit, sans-serif;">Asset Management System</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,7 +11,7 @@
       <a class="nav-item nav-link active" href="/">Home</a>
       <a class="nav-item nav-link" href="assets.php">List Assets</a>
       <a class="nav-item nav-link" href="insert_asset.php">Insert Asset</a>
-      <?php if ($_SESSION['user_type'] == 'superadmin') { echo '<a class="nav-item nav-link" href="register_user.php">Register User</a>'; } ?>
+      <?php if ($_SESSION['user_type'] == 'superadmin') { echo $register; } ?>
       <a class="nav-item nav-link" href="logout.php">Logout</a>
     </div>
   </div>
