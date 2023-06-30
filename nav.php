e601443ce2d7a66b6773002c43eeec749d5a8881
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-30 05:22:58 +0000

    nav and logout update

--- nav.php
+++ nav.php
@@ -1,15 +1,14 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark noprint">
-  <a class="navbar-brand" href="#" style="font-family: Kanit, sans-serif;">Student Information System</a>
+  <a class="navbar-brand" href="#" style="font-family: Kanit, sans-serif;">Asset Management System</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link active" href="/">Home</a>
-      <a class="nav-item nav-link" href="/SIS/forms/cashier/addpayment.php">Add Payment</a>
-      <a class="nav-item nav-link" href="/SIS/forms/cashier/precord.php">Payment Records</a>
-      <a class="nav-item nav-link" href="/SIS/about.php">About</a>
-      <a class="nav-item nav-link" href="/SIS/logout.php">Logout</a>
+      <a class="nav-item nav-link" href="assets.php">List Assets</a>
+      <a class="nav-item nav-link" href="insert_asset.php">Insert Asset</a>
+      <a class="nav-item nav-link" href="logout.php">Logout</a>
     </div>
   </div>
   <span class="navbar-text" style="font-family: Kanit, sans-serif;">
