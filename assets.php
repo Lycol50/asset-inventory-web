692d686a3d1748a02fbe0cfa876747615fd66729
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-13 11:20:15 +0000

    try 8

--- assets.php
+++ assets.php
@@ -79,11 +79,9 @@ function showdocuments($param) {
                                 <td>" . $row["date_acquired"] . "</td>
                                 <td>" . $row["asset_tag"] . "</td>
                                 <td>" . $row["location_asset"] . "</td>
-                                <td>
-                                <?php
+                                <td>";
                                     showdocuments($row["documents"]);
-                                ?>
-                                </td>
+                                echo "</td>
                                 <td class='d-print-none'>
                                     <a href='update_asset.php?asset_id=" . $row["asset_tag"] . "' class='btn btn-sm btn-outline-secondary'>Edit</a>
                                     <a href='delete_asset.php?asset_id=" . $row["asset_tag"] . "' class='btn btn-sm btn-outline-secondary'>Delete</a>
