086ed11a84b30b9f095bdb368656c0a00927898e
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-13 16:50:36 +0000

    eafaf

--- print_assets.php
+++ print_assets.php
@@ -18,7 +18,7 @@ function generatePDFAndPrint($logoPath, $assetTags)
     
     // Set Y position for asset tags
     $yPosition = 15;
-    $xPosition = 20; // Adjust the x-axis position here
+    $xPosition = 10; // Adjust the x-axis position here
 
     // Output asset tags
     foreach ($assetTags as $assetTag) {
