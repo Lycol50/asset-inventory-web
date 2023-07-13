0ec7f79a21789346b89e9b7ea031d3ae7165156c
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-13 14:33:17 +0000

    test

--- dashboard.php
+++ dashboard.php
@@ -31,9 +31,8 @@ if (!isset($_SESSION['loggedin'])) {
             <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
                 <div class="card-header">Header</div>
                 <div class="card-body">
-                    <h5 class="card-title">Primary card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
+                    <h3 class="card-title">Primary card title</h3>
+                    <h5 class="card-text">All Assets in this building</h5>
                 </div>
             </div>
             <div class="card text-white bg-primary mb-3">
