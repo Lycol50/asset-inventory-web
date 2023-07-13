538d9fb014df173b13c3873964e3b30ba2a0a538
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-13 12:16:51 +0000

    up

--- update_asset.php
+++ update_asset.php
@@ -2,29 +2,34 @@
 include 'config.php';
 session_start();
 
-if (!isset($_SESSION['loggedin'])) {
-	header('Location: login.php');
-	exit;
+if (!isset($_SESSION['loggedin']))
+{
+    header('Location: login.php');
+    exit;
 }
 
-if ($_SESSION['account_type'] !== "admin" && $_SESSION['account_type'] !== "superadmin") {
+if ($_SESSION['account_type'] !== "admin" && $_SESSION['account_type'] !== "superadmin")
+{
     header('Location: dashboard.php');
     echo "<script>alert('You do not have permission to access this page.')</script>";
 }
-
 
 // insert variables for asset
 $brand_err = $model_err = $serial_number_err = $status_err = $equipment_name_err = $location_err = $price_value_err = $date_acquired_err = $assettype_err = $remarks_err = "";
 
 // get the asset id from the url parameter
-if (isset($_GET['id'])) {
+if (isset($_GET['id']))
+{
     $id = $_GET['id'];
     $sql = "SELECT * FROM assets WHERE id = ?";
-    if ($stmt = $mysqli->prepare($sql)) {
+    if ($stmt = $mysqli->prepare($sql))
+    {
         $stmt->bind_param("i", $id);
-        if ($stmt->execute()) {
+        if ($stmt->execute())
+        {
             $result = $stmt->get_result();
-            if ($result->num_rows == 1) {
+            if ($result->num_rows == 1)
+            {
                 $row = $result->fetch_array(MYSQLI_ASSOC);
                 $asset_type = $row['asset_type'];
                 $brand = $row['brand'];
@@ -36,177 +41,148 @@ if (isset($_GET['id'])) {
                 $price_value = $row['price_value'];
                 $date_acquired = $row['date_acquired'];
                 $remarks = $row['remarks'];
-            } else {
-                echo "<script>alert('Something went wrong. Please try again!')</script>";
-            }
-        } else {
-            echo "<script>alert('Something went wrong. Please try again!')</script>";
-        }
-    }
-    $stmt->close();
-}
 
-// Processing form data when form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                // process the form with new variables
+                if ($_SERVER["REQUEST_METHOD"] == "POST")
+                {
 
-    // check if asset type is empty
-    if (empty(trim($_POST["asset_type"]))) {
-        $assettype_err = "Please enter asset type.";
-    } else {
-        $asset_type = trim($_POST["asset_type"]);
-    }
+                    // check if asset type is empty
+                    if (empty(trim($_POST["asset_type"])))
+                    {
+                        $assettype_err = "Please enter asset type.";
+                    }
+                    else
+                    {
+                        $asset_type = trim($_POST["asset_type"]);
+                    }
 
-    // check if brand is empty
-    if (empty(trim($_POST["brand"]))) {
-        $brand_err = "Please enter brand.";
-    } else {
-        $brand = trim($_POST["brand"]);
-    }
+                    // check if brand is empty
+                    if (empty(trim($_POST["brand"])))
+                    {
+                        $brand_err = "Please enter brand.";
+                    }
+                    else
+                    {
+                        $brand = trim($_POST["brand"]);
+                    }
 
-    // check if model is empty
-    if (empty(trim($_POST["model"]))) {
-        $model_err = "Please enter model.";
-    } else {
-        $model = trim($_POST["model"]);
-    }
+                    // check if model is empty
+                    if (empty(trim($_POST["model"])))
+                    {
+                        $model_err = "Please enter model.";
+                    }
+                    else
+                    {
+                        $model = trim($_POST["model"]);
+                    }
 
-    // check if serial number is empty
-    if (empty(trim($_POST["serial_number"]))) {
-        $serial_number_err = "Please enter serial number.";
-    } else {
-        $serial_number = trim($_POST["serial_number"]);
-    }
+                    // check if serial number is empty
+                    if (empty(trim($_POST["serial_number"])))
+                    {
+                        $serial_number_err = "Please enter serial number.";
+                    }
+                    else
+                    {
+                        $serial_number = trim($_POST["serial_number"]);
+                    }
 
-    // check if status is empty
-    if (empty(trim($_POST["status"]))) {
-        $status_err = "Please enter status.";
-    } else {
-        $status = trim($_POST["status"]);
-    }
+                    // check if status is empty
+                    if (empty(trim($_POST["status"])))
+                    {
+                        $status_err = "Please enter status.";
+                    }
+                    else
+                    {
+                        $status = trim($_POST["status"]);
+                    }
 
-    // check if equipment name is empty
-    if (empty(trim($_POST["equipment_name"]))) {
-        $equipment_name_err = "Please enter equipment name.";
-    } else {
-        $equipment_name = trim($_POST["equipment_name"]);
-    }
+                    // check if equipment name is empty
+                    if (empty(trim($_POST["equipment_name"])))
+                    {
+                        $equipment_name_err = "Please enter equipment name.";
+                    }
+                    else
+                    {
+                        $equipment_name = trim($_POST["equipment_name"]);
+                    }
 
-    // check if location is empty
-    if (empty(trim($_POST["location_asset"]))) {
-        $location_err = "Please enter location.";
-    } else {
-        $location_asset = trim($_POST["location_asset"]);
-    }
+                    // check if location is empty
+                    if (empty(trim($_POST["location_asset"])))
+                    {
+                        $location_err = "Please enter location.";
+                    }
+                    else
+                    {
+                        $location_asset = trim($_POST["location_asset"]);
+                    }
 
-    // check if price value is empty
-    if (empty(trim($_POST["price_value"]))) {
-        $price_value_err = "Please enter price value.";
-    } else {
-        $price_value = trim($_POST["price_value"]);
-    }
+                    // check if price value is empty
+                    if (empty(trim($_POST["price_value"])))
+                    {
+                        $price_value_err = "Please enter price value.";
+                    }
+                    else
+                    {
+                        $price_value = trim($_POST["price_value"]);
+                    }
 
-    // check if date acquired is empty
-    if (empty(trim($_POST["date_acquired"]))) {
-        $date_acquired_err = "Please enter date acquired.";
-    } else {
-        $date_acquired = trim($_POST["date_acquired"]);
-    }
+                    // check if date acquired is empty
+                    if (empty(trim($_POST["date_acquired"])))
+                    {
+                        $date_acquired_err = "Please enter date acquired.";
+                    }
+                    else
+                    {
+                        $date_acquired = trim($_POST["date_acquired"]);
+                    }
 
-    // check if remarks is empty
-    if (empty(trim($_POST["remarks"]))) {
-        $remarks_err = "Please enter remarks.";
-    } else {
-        $remarks = trim($_POST["remarks"]);
-    }
-    
-    function generateRandomLetter($length = 1) {
-        $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[random_int(0, $charactersLength - 1)];
-        }
-        return $randomString;
-    }
+                    // check if remarks is empty
+                    if (empty(trim($_POST["remarks"])))
+                    {
+                        $remarks_err = "Please enter remarks.";
+                    }
+                    else
+                    {
+                        $remarks = trim($_POST["remarks"]);
+                    }
 
-    // $asset_number = "ICNG-" . substr($date_acquired, 0, 4) . "-" . substr(md5($serial_number), 0, 4) . generateRandomLetter();
+                    // submit everything to db
+                    if (empty($brand_err) && empty($model_err) && empty($serial_number_err) && empty($status_err) && empty($equipment_name_err) && empty($location_err) && empty($price_value_err) && empty($date_acquired_err) && empty($assettype_err))
+                    {
+                        // prepare an insert statement
+                        $sql = "INSERT INTO assets (brand, model, serial_number, status, equipment_name, location_asset, price_value, date_acquired, remarks, asset_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,)";
 
-    // remove functionality to upload documents because it is impossible to reupdate the documents
-    /* prepare to upload documents to server with foreach
-    $target_dir = "uploads/";
-    $filenames = array_filter($_FILES['documents']['name']);
-    if (!empty($filenames)) {
-        foreach ($_FILES['documents']['name'] as $key=>$val) {
-            $filename = basename($_FILES['files']['name'][$key]); 
-            $targetFilePath = $targetDir . $filename;
-            move_uploaded_file($_FILES["documents"]["tmp_name"][$key], $targetFilePath);
-        }
-    }
-    
-    $countfiles = count($_FILES['documents']['name']);
-    $totalFileUploaded = 0;
-    for($i=0;$i<$countfiles;$i++){
-        $filename = $_FILES['documents']['name'][$i];
+                        if ($stmt = $mysqli->prepare($sql))
+                        {
+                            // bind variables to the prepared statement as parameters
+                            $stmt->bind_param("ssssssssss", $param_brand, $param_model, $param_serial_number, $param_status, $param_equipment_name, $param_location, $param_price_value, $param_date_acquired, $param_remarks, $param_asset_type);
 
-        ## Location
-        $location = "uploads/".$filename;
-        $extension = pathinfo($location,PATHINFO_EXTENSION);
-        $extension = strtolower($extension);
+                            // set parameters
+                            $param_asset_type = $asset_type;
+                            $param_brand = $brand;
+                            $param_model = $model;
+                            $param_serial_number = $serial_number;
+                            $param_status = $status;
+                            $param_equipment_name = $equipment_name;
+                            $param_location = $location_asset;
+                            $param_price_value = $price_value;
+                            $param_date_acquired = $date_acquired;
+                            $param_remarks = $remarks;
 
-        ## File upload allowed extensions
-        $valid_extensions = array("jpg","jpeg","png","pdf","docx");
+                            // attempt to execute the prepared statement
+                            if ($stmt->execute())
+                            {
+                                echo '<script>alert("Assets updated successfully");</script>';
 
-        $response = 0;
-        ## Check file extension
-        if(in_array(strtolower($extension), $valid_extensions)) {
-             ## Upload file
-             if(move_uploaded_file($_FILES['documents']['tmp_name'][$i],$location)){
-                  $totalFileUploaded++;
-             }
-        }
-   } */
-    
-    // submit everything to db
-    if (empty($brand_err) && empty($model_err) && empty($serial_number_err) && empty($status_err) && empty($equipment_name_err) && empty($location_err) && empty($price_value_err) && empty($date_acquired_err) && empty($assettype_err)) {
-        // prepare an insert statement
-        $sql = "INSERT INTO assets (brand, model, serial_number, status, equipment_name, location_asset, price_value, date_acquired, remarks, asset_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-        if ($stmt = $mysqli->prepare($sql)) {
-            // bind variables to the prepared statement as parameters
-            $stmt->bind_param("ssssssssss", $param_brand, $param_model, $param_serial_number, $param_status, $param_equipment_name, $param_location, $param_price_value, $param_date_acquired, $param_remarks, $param_asset_type);
-
-            // set parameters
-            $param_asset_type = $asset_type;
-            $param_brand = $brand;
-            $param_model = $model;
-            $param_serial_number = $serial_number;
-            $param_status = $status;
-            $param_equipment_name = $equipment_name;
-            $param_location = $location_asset;
-            $param_price_value = $price_value;
-            $param_date_acquired = $date_acquired;
-            $param_remarks = $remarks;
-
-            // attempt to execute the prepared statement
-            if ($stmt->execute()) {
-                echo '<script>alert("Asset added successfully\nTotal File uploaded : ' . $totalFileUploaded . '");</script>';
-
-            } else {
-                echo '<script>alert("Something went wrong.");</script>';
-            }
-        }
-
-        // close statement
-        $stmt->close();
-    }
-
-    // close connection
-    $mysqli->close();
-}
+                            }
+                            else
+                            {
+                                echo '<script>alert("Something went wrong.");</script>';
+                            }
+                        }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <title>Asset Management System</title>
@@ -219,13 +195,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </script>
     <link rel="icon" type="image/x-icon" href="white.png">
 </head>
-
 <body>
     <?php include 'nav.php'; ?>
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Update Asset</h1>
+                <h1>Add Asset</h1>
 
                 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" autocomplete="off">
                     <label for="asset_type">Asset Type</label>
@@ -290,9 +265,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         value="<?php echo $date_acquired; ?>">
                     <span class="invalid-feedback"><?php echo $date_acquired_err; ?></span>
                     <br>
-                    <!-- <label for="documents">Documents</label>
-                    <input type="file" name="documents[]" id="documents" class="form-control" multiple>
-                    <br> -->
                     <label for="remarks">Remarks</label>
                     <input type="text" name="remarks" id="remarks"
                         class="form-control <?php echo (!empty($remarks_err)) ? 'is-invalid' : ''; ?>"
@@ -306,5 +278,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </div>
 </body>
-
 </html>
+
+<?php
+                        // close statement
+                        $stmt->close();
+                    }
+
+                    // close connection
+                    $mysqli->close();
+                }
+            }
+            else
+            {
+                echo "<script>alert('Something went wrong. Please try again!')</script>";
+            }
+        }
+        else
+        {
+            echo "<script>alert('Something went wrong. Please try again!')</script>";
+        }
+    }
+    $stmt->close();
+}
+
