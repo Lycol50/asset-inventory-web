ce5edb0a0af3a964f45de8e606f4b6058e75b0c3
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-13 16:39:27 +0000

    adjust

--- print_assets.php
+++ print_assets.php
@@ -1,6 +1,3 @@
-<?php
-require('fpdf/fpdf.php');
-
 // Function to generate PDF and print immediately
 function generatePDFAndPrint($logoPath, $assetTags)
 {
@@ -16,9 +13,14 @@ function generatePDFAndPrint($logoPath, $assetTags)
     // Set logo
     $pdf->Image($logoPath, 10, 10, 30);
     
+    // Set Y position for asset tags
+    $yPosition = 50;
+    
     // Output asset tags
     foreach ($assetTags as $assetTag) {
+        $pdf->SetXY(50, $yPosition);
         $pdf->Cell(0, 10, $assetTag, 0, 1);
+        $yPosition += 10; // Increase Y position for the next asset tag
     }
     
     // Output PDF
@@ -27,15 +29,3 @@ function generatePDFAndPrint($logoPath, $assetTags)
     // Initiate print immediately
     echo "<script type='text/javascript'>window.print();</script>";
 }
-
-// Get the selected asset tags from the URL
-$selectedAssets = $_GET['selectedAssets'];
-
-// Convert the selected asset tags to an array
-$assetTags = explode(',', $selectedAssets);
-
-// Example usage
-$logoPath = 'logo_info.png';
-
-// Generate PDF and print
-generatePDFAndPrint($logoPath, $assetTags);
