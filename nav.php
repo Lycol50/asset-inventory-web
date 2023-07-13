ebc0ebed0279885f5a129f6b2cff821d4105892a
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-13 10:30:41 +0000

    try

--- nav.php
+++ nav.php
@@ -3,7 +3,7 @@ $register = '<a class="nav-item nav-link" href="register_user.php">Register User
 $insert_asset = '<a class="nav-item nav-link" href="insert_asset.php">Insert Asset</a>';
 ?>
 <nav class="noprint navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#" style="font-family: Kanit, sans-serif;">Asset Management System</a>
+  <a class="noprint navbar-brand" href="#" style="font-family: Kanit, sans-serif;">Asset Management System</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
