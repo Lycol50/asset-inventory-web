d8be6239b1fb87f2bf9c3282f6b84b8b05fcaaf0
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-13 23:52:10 +0000

    print

--- assets.php
+++ assets.php
@@ -92,7 +92,7 @@ if (!isset($_SESSION['loggedin'])) {
                 </script>
                 <br>
                 <!-- table for assets -->
-                <div class="table-responsive">
+                <div class="table-responsive d-print-none">
                     <table class="table table-striped table-bordered border-start" id="assetsTable">
                         <thead>
                             <tr>
