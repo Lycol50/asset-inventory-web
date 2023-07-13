2020cb9a2ed2e5587ca6cce165798e0b984c27db
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-13 14:48:39 +0000

    col

--- dashboard.php
+++ dashboard.php
@@ -28,55 +28,55 @@ if (!isset($_SESSION['loggedin'])) {
     <div class="container">
     <h1>Dashboard</h1>
         <div class="row row-cols-1 row-cols-md-3">
-            <div class="col">
+            <div class="col align-items-stretch">
                 <div class="card text-white bg-primary mb-3" style="max-width: 20rem; text-align:center;">
                     <div class="card-header">Header</div>
-                    <div class="card-body">
+                    <div class="card-body d-flex flex-column">
                         <h3 class="card-title">Primary card title</h3>
                         <h5 class="card-text">All Assets in this building</h5>
                     </div>
                 </div>
             </div>
-            <div class="col">
+            <div class="col align-items-stretch">
                 <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
                     <div class="card-header">Header</div>
-                    <div class="card-body">
+                    <div class="card-body d-flex flex-column">
                         <h3 class="card-title">Primary card title</h3>
                         <h5 class="card-text">In Use Assets in this building</h5>
                     </div>
                 </div>
             </div>
-            <div class="col">
+            <div class="col align-items-stretch">
                 <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
                     <div class="card-header">Header</div>
-                    <div class="card-body">
+                    <div class="card-body d-flex flex-column">
                         <h3 class="card-title">Primary card title</h3>
-                        <h5 class="card-text" style="font-size:5vw;">In Storage Assets in this building</h5>
+                        <h5 class="card-text">In Storage Assets in this building</h5>
                     </div>
                 </div>
             </div>
-            <div class="col">
+            <div class="col align-items-stretch">
                 <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
                     <div class="card-header">Header</div>
-                    <div class="card-body">
+                    <div class="card-body d-flex flex-column">
                         <h3 class="card-title">Primary card title</h3>
                         <h5 class="card-text">For Repair Assets in this building</h5>
                     </div>
                 </div>
             </div>
-            <div class="col">
+            <div class="col align-items-stretch">
                 <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
                     <div class="card-header">Header</div>
-                    <div class="card-body">
+                    <div class="card-body d-flex flex-column">
                         <h3 class="card-title">Primary card title</h3>
                         <h5 class="card-text">For Disposal Assets in this building</h5>
                     </div>
                 </div>
             </div>
-            <div class="col">
+            <div class="col align-items-stretch">
                 <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
                     <div class="card-header">Header</div>
-                    <div class="card-body">
+                    <div class="card-body d-flex flex-column">
                         <h3 class="card-title">Primary card title</h3>
                         <h5 class="card-text">Total Assets in this building</h5>
                     </div>
