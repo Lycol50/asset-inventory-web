51b552fda866d2f7730bbf84a31899a9223e842f
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-13 15:10:10 +0000

    wewe

--- dashboard.php
+++ dashboard.php
@@ -38,11 +38,13 @@ if (!isset($_SESSION['loggedin'])) {
                         </div>
                     </div>
                 </div>
-                <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
-                    <div class="card-header">Header</div>
-                    <div class="card-body d-flex flex-column">
-                        <h3 class="card-title">Primary card title</h3>
-                        <h5 class="card-text">In Use Assets in this building</h5>
+                <div class="col">
+                    <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body d-flex flex-column">
+                            <h3 class="card-title">Primary card title</h3>
+                            <h5 class="card-text">In Use Assets in this building</h5>
+                        </div>
                     </div>
                 </div>
                 <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
