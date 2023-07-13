75b696e49b94e6a245b3b0349c62c81f4d9446ec
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-13 14:42:34 +0000

    3

--- dashboard.php
+++ dashboard.php
@@ -27,7 +27,7 @@ if (!isset($_SESSION['loggedin'])) {
     <?php include 'nav.php'; ?>
     <div class="container">
     <h1>Dashboard</h1>
-        <div class="row row-cols-1 row-cols-md-2">
+        <div class="row row-cols-1 row-cols-md-3">
             <div class="col">
                 <div class="card text-white bg-primary mb-3" style="max-width: 20rem; text-align:center;">
                     <div class="card-header">Header</div>
