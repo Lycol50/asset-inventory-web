0a6f0c0b93b79b740e231d4bcd92b97ab522ba1f
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-13 15:06:18 +0000

    card-deck

--- dashboard.php
+++ dashboard.php
@@ -26,59 +26,61 @@ if (!isset($_SESSION['loggedin'])) {
 <body>
     <?php include 'nav.php'; ?>
     <div class="container">
-    <h1>Dashboard</h1><br>
+        <h1>Dashboard</h1><br>
         <div class="row row-cols-1 row-cols-md-3">
-            <div class="col d-flex align-items-stretch">
-                <div class="card text-white bg-primary mb-3" style="max-width: 20rem; text-align:center;">
-                    <div class="card-header">Header</div>
-                    <div class="card-body d-flex flex-column">
-                        <h3 class="card-title">Primary card title</h3>
-                        <h5 class="card-text">All Assets in this building</h5>
+            <div class="card-deck">
+                <div class="col d-flex align-items-stretch">
+                    <div class="card text-white bg-primary mb-3" style="max-width: 20rem; text-align:center;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body d-flex flex-column">
+                            <h3 class="card-title">Primary card title</h3>
+                            <h5 class="card-text">All Assets in this building</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col d-flex align-items-stretch">
-                <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
-                    <div class="card-header">Header</div>
-                    <div class="card-body d-flex flex-column">
-                        <h3 class="card-title">Primary card title</h3>
-                        <h5 class="card-text">In Use Assets in this building</h5>
+                <div class="col d-flex align-items-stretch">
+                    <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body d-flex flex-column">
+                            <h3 class="card-title">Primary card title</h3>
+                            <h5 class="card-text">In Use Assets in this building</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col d-flex align-items-stretch">
-                <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
-                    <div class="card-header">Header</div>
-                    <div class="card-body d-flex flex-column">
-                        <h3 class="card-title">Primary card title</h3>
-                        <h5 class="card-text">In Storage Assets in this building</h5>
+                <div class="col d-flex align-items-stretch">
+                    <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body d-flex flex-column">
+                            <h3 class="card-title">Primary card title</h3>
+                            <h5 class="card-text">In Storage Assets in this building</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col d-flex align-items-stretch">
-                <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
-                    <div class="card-header">Header</div>
-                    <div class="card-body d-flex flex-column">
-                        <h3 class="card-title">Primary card title</h3>
-                        <h5 class="card-text">For Repair Assets in this building</h5>
+                <div class="col d-flex align-items-stretch">
+                    <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body d-flex flex-column">
+                            <h3 class="card-title">Primary card title</h3>
+                            <h5 class="card-text">For Repair Assets in this building</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col d-flex align-items-stretch">
-                <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
-                    <div class="card-header">Header</div>
-                    <div class="card-body d-flex flex-column">
-                        <h3 class="card-title">Primary card title</h3>
-                        <h5 class="card-text">For Disposal Assets in this building</h5>
+                <div class="col d-flex align-items-stretch">
+                    <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body d-flex flex-column">
+                            <h3 class="card-title">Primary card title</h3>
+                            <h5 class="card-text">For Disposal Assets in this building</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col d-flex align-items-stretch">
-                <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
-                    <div class="card-header">Header</div>
-                    <div class="card-body d-flex flex-column">
-                        <h3 class="card-title">Primary card title</h3>
-                        <h5 class="card-text">Total Assets in this building</h5>
+                <div class="col d-flex align-items-stretch">
+                    <div class="card text-white bg-success mb-3" style="max-width: 20rem; text-align:center;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body d-flex flex-column">
+                            <h3 class="card-title">Primary card title</h3>
+                            <h5 class="card-text">Total Assets in this building</h5>
+                        </div>
                     </div>
                 </div>
             </div>
