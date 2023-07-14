d96d5ceb7994b60a9e99d0fc0b8cc18895ee22ab
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-14 03:03:19 +0000

    test

--- delete_asset.php
+++ delete_asset.php
@@ -1,3 +1,7 @@
+<html>
+    <head>
+</head>
+<body>
 <?php
 //include config
 require_once('config.php');
@@ -10,7 +14,7 @@ if (!isset($_SESSION['loggedin'])) {
 }
 
 // check if user has permission to view only admin or superadmin can view this page
-if ($_SESSION['account_type'] === "user") {
+if ($_SESSION['account_type'] === "admin" && $_SESSION['account_type'] !== "superadmin") {
     echo '<script type="text/javascript">window.onload=function(){alert("You do not have permission to access this page.");};</script>';    header('Location: assets.php');
     exit;
 }
@@ -31,4 +35,6 @@ if (isset($_GET['asset_tag'])) {
     }
     $stmt->close();
 }
-
+?>
+</body>
+</html>
