2e4563dcd990d9fd6733813cfafec60d20f2f35f
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-23 17:17:18 +0000

    last

--- config.php
+++ config.php
@@ -50,8 +50,9 @@ if($mysqli === false){
         `documents` text NOT NULL,
         `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
         `updated_at` text NOT NULL,
-        `user_id` int(13) NOT NULL FOREIGN KEY REFERENCES users(user_id),
-        PRIMARY KEY (`asset_id`)
+        `user_id` int(255) NOT NULL,
+        PRIMARY KEY (`asset_id`),
+        FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`)
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
     // execute query
     if ($mysqli->query($sql) === TRUE) {
