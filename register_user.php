f469da432f0b8cd7ecb520f822b6b156ff6adc75
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-23 18:06:07 +0000

    dup

--- register_user.php
+++ register_user.php
@@ -101,6 +101,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                      // insert password reset code into password_reset table
             $sql2 = "INSERT INTO password_reset (password_reset_code, user_id) VALUES (?, ?)";
             if ($stmt = $mysqli->prepare($sql2)) {
+                $username = $_POST['username'];
                 // bind variables to the prepared statement as parameters
                 $stmt->bind_param("si", $param_password_reset_code, $param_user_id);
                 
