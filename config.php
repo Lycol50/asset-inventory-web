25fee176422b2e1476ebffcec908298104d3a8ad
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-07-23 17:14:42 +0000

    last

--- config.php
+++ config.php
@@ -50,7 +50,7 @@ if($mysqli === false){
         `documents` text NOT NULL,
         `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
         `updated_at` text NOT NULL,
-        `user_id` int(13) NOT NULL FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+        `user_id` int(13) NOT NULL FOREIGN KEY REFERENCES users(user_id),
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
     // execute query
     if ($mysqli->query($sql) === TRUE) {
