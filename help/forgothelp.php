d6bc05d7206d37cd725539c1bccd24338e653678
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-08-08 12:54:19 +0000

    last help

---
+++ help/forgothelp.php
@@ -0,0 +1,57 @@
+<?php
+include dirname(__FILE__, 2) . '/config.php';
+
+session_start();
+if (!isset($_SESSION['loggedin'])) {
+    header("Location: dirname(__FILE__, 2) . 'login.php'");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Asset Management System</title>
+    <link rel="stylesheet" href="../style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <link rel="icon" type="image/x-icon" href="white.png">
+    <link href="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.css" rel="stylesheet">
+    <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.js"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/extensions/print/bootstrap-table-print.min.js"></script>
+</head>
+
+<body>
+    <?php include 'nav.php'; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Forgotting Password Guide</h1>
+                <p>Here the instruction on how to reset/change your password. <b>This will require human interaction to
+                        users of this system</b></p>
+                <br>
+                <h3>Obtaining the Password Reset Code</h3>
+                <div class="alert alert-warning" role="alert">
+                    <p>You must have knowledge on using MySQL/MariaDB on terminal/command-line, and/or GUI <b>OR</b> Must have knowledge on using PHPMyAdmin (it is built-in on XAMPP)</p>
+                </div>
+                <ol>
+                    <li>Open your MySQL/MariaDB terminal/command-line, and/or GUI <b>OR</b> Open PHPMyAdmin</li>
+                    <li>Open the database of this system</li>
+                    <li>Open the <code>users</code> table</li>
+                    <li>Find the user that you want to reset/change the password</li>
+                    <li>Remember the value of the <code>id</code> column</li>
+                    <li>Open the <code>password_reset</code> table</li>
+                    <li>Search the id you search before from users table</li>
+                    <li>Write the <code>password_reset_code</code> for that user</li>
+                    <li>Hand-over to the user his password reset code, and tell that do not lose it.</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</body>
---
+++ help/register.php
@@ -0,0 +1,93 @@
+<?php
+include dirname(__FILE__, 2) . '/config.php';
+
+session_start();
+if (!isset($_SESSION['loggedin'])) {
+    header("Location: dirname(__FILE__, 2) . 'login.php'");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Asset Management System</title>
+    <link rel="stylesheet" href="../style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <link rel="icon" type="image/x-icon" href="white.png">
+    <link href="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.css" rel="stylesheet">
+    <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.js"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/extensions/print/bootstrap-table-print.min.js"></script>
+</head>
+
+<body>
+    <?php include 'nav.php'; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Registering User to the System</h1>
+                <p>Clicking the <b>Register User</b> above of Navigation bar, it will go to the Register User Page</p>
+                <br>
+                <img class="img-fluid" src="imgs/8.png"><br>
+                <br>
+                <h3>Adding User to the System</h3>
+                <ol>
+                    <li>Insert a preferred username on <b>username box</b></li>
+                    <li>Insert the <b>First Name</b> of the new user</li>
+                    <li>Insert the <b>Last Name</b> of the new user</li>
+                    <li>Insert the desired password for that user on <b>Password box</b></li>
+                    <div class="alert alert-warning" role="alert">
+                        <p>Minimum Characters for Password is 6 only, you can use alphanumeric, symbols characters</p>
+                    </div>
+                    <li>Insert the password again on <b>Confirm Password box</b></li>
+                    <li>Select User Type for that user</li>
+                    <div class="alert alert-info" role="alert">
+                        <p>Here are the Permissions Table for user</p>
+                        <table>
+                            <tr>
+                                <th>Permissions</th>
+                                <th>Admin</th>
+                                <th>User</th>
+                            </tr>
+                            <tr>
+                                <td>View and Print Assets</td>
+                                <td>✅</td>
+                                <td>✅</td>
+                            </tr>
+                            <tr>
+                                <td>Add Asset</td>
+                                <td>✅</td>
+                                <td>❌</td>
+                            </tr>
+                            <tr>
+                                <td>Edit Asset</td>
+                                <td>✅</td>
+                                <td>❌</td>
+                            </tr>
+                            <tr>
+                                <td>Delete Asset</td>
+                                <td>✅</td>
+                                <td>❌</td>
+                            </tr>
+                            <tr>
+                                <td>Add User</td>
+                                <td>❌</td>
+                                <td>❌</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <li>Click <b>Register</b> button to add the user to the system</li>
+                    <li>A popout will appear that the new user has been registered.</li>
+                    <li>Click <b>Go to Dashboard</b> to go back to Dashboard</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</body>
