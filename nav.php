9f0ff2e5636e8493dc74b7953784abafb9c3ac14
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-08-08 08:56:31 +0000

    img nav

--- nav.php
+++ nav.php
@@ -3,7 +3,7 @@ $register = '<a class="nav-item nav-link" href="register_user.php">Register User
 $insert_asset = '<a class="nav-item nav-link" href="insert_asset.php">Insert Asset</a>';
 ?>
 <nav class="d-print-none navbar navbar-expand-lg navbar-dark bg-dark">
-  <img src="whitehttps://em-content.zobj.net/source/joypixels/340/regional-indicator-symbol-letter-i_1f1ee.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
+  <img src="https://em-content.zobj.net/source/joypixels/340/regional-indicator-symbol-letter-i_1f1ee.png" alt="" width="30" height="30" class="d-inline-block align-text-top">&nbsp;&nbsp;
   <a class="navbar-brand" href="#" style="font-family: Kanit, sans-serif;">Asset Inventory System</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
